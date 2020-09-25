2478eefbb274bb2f2c969d6cd1d39319c24bad77
Mario Stocco <mario@advantia.ca>
2020-09-25 09:15:35 -0700

    Shrink the agencyvc class font-size to 0.85em

--- application/templateDocument.py
+++ application/templateDocument.py
@@ -113,7 +113,7 @@ class TemplateDocument(HTML5Document):
 		self.opengraph.url = 'https://mariostoc.co%s' % self.documentURI
 		self.head.append(self.opengraph)
 		attrs = {'rel':'stylesheet','type':'text/css','media':'screen'}
-		for href in ['flickity.min.css','mstocco.css?v=006']:
+		for href in ['flickity.min.css','mstocco.css?v=007']:
 			attrs['href'] = '/assets/css/%s' % href
 			self.head.append(LINK(attrs))
 		
